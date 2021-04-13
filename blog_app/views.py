821f272e06c439012d5667a5cbaf58d7ca3af689
JKK86 <j.kurowski86@wp.pl>
2021-04-13 22:37:48 +0200

    post_list view based on generic ListView

--- blog_app/views.py
+++ blog_app/views.py
@@ -5,10 +5,8 @@ from django.views.generic import ListView
 from blog_app.models import Post, PUBLISHED
 
 
-class PostListView(View):
-    def get(self, request):
-        posts = Post.objects.filter(status=PUBLISHED)
-        return render(request, 'blog/post_list.html', {'posts': posts})
-
-
-# class PostListView2(ListView):
+class PostListView(ListView):
+    queryset = Post.objects.filter(status=PUBLISHED)
+    context_object_name = 'posts'
+    paginate_by = 5
+    template_name = 'blog/post_list.html'
