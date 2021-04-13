72e6ca62215ca3d9c1479ebd633a181e1bb48927
JKK86 <j.kurowski86@wp.pl>
2021-04-13 19:43:30 +0200

    register post model in admin panel

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog_app.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'published', 'created', 'updated', 'status']
+    list_filter = ['status', 'published', 'created', 'updated', 'author']
+    search_fields = ['title']
+    prepopulated_fields = {'slug': ('title',)}
+    exclude = ['created', 'updated', 'published']
+    list_editable = ['status']
