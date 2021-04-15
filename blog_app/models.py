dd7fc410c4283670b954c6e2f1179c2099fa44cd
JKK86 <j.kurowski86@wp.pl>
2021-04-15 19:47:29 +0200

    add comment model

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,8 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 
 from blog import settings
+
+User = get_user_model()
 
 DRAFT = "draft"
 PUBLISHED = "published"
@@ -41,3 +44,20 @@ class Post(models.Model):
 
     class Meta:
         ordering = ('-publish', )
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = "komentarz"
+        verbose_name_plural = "Komentarze"
+        ordering = ('-created', )
